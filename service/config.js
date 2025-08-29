8cdaaeb594a1eae4265d03dc5451cafcedbd7f26
npvedran <vedran@net-projekt.hr>
2025-08-29 20:46:15 +0200

    Update config.js

--- service/config.js
+++ service/config.js
@@ -1,8 +1,15 @@
 export const llmEngine = "llama3.1";
-export const llmCommand =
-  `Create a one-sentence english summary of the text. 
+export const llmCommand = `Create a one-sentence english summary of the text. 
   Be sarcastic. 
   Do not say you are sarcastic. 
   Do not say you are a chatbot.
   Do not say you are a language model.
-  Remove quotes from beginning and the end.`;
+  Remove quotes from beginning and the end.
+  If there's no text - say: "There's nothing here..."`;
+
+export const dbConnectionDetails = {
+  host: "localhost",
+  user: "***",
+  password: "***",
+  database: "repo",
+};
